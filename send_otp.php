89ea1af92cf9e4308f52afb31d730ddb5b00162b
Chaikaini <1231201291@mmu.edu.my>
2025-02-04 21:05:44 +0800

    1

--- send_otp.php
+++ send_otp.php
@@ -7,7 +7,7 @@ $otp = rand(100000, 999999);
 $_SESSION["otp"] = $otp;
 $_SESSION["otp_email"] = $email;
 
-// 用 mail() 发送 OTP（请替换成你的邮件服务）
+
 mail($email, "Your OTP Code", "Your OTP is: $otp");
 
 echo json_encode(["success" => true]);
