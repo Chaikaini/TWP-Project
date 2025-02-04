8ae523e98830d2ffb9d31246c57d31654479fda5
Chaikaini <1231201291@mmu.edu.my>
2025-02-04 19:33:19 +0800

    1

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "user_information"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $phone_number = $_POST['phone_number'];
+    $gender = $_POST['gender'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // 加密密码
+
+    $sql = "INSERT INTO users (username, email, address, phone_number, gender, password) 
+            VALUES ('$username', '$email', '$address', '$phone_number', '$gender', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
