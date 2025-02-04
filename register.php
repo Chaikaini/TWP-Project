2cf8cfa6feca784862c47a2153f4d86ba10a2d7f
Chaikaini <1231201291@mmu.edu.my>
2025-02-04 19:45:15 +0800

    1

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "user_information"; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_information";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -16,18 +16,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $address = $_POST['address'];
     $phone_number = $_POST['phone_number'];
     $gender = $_POST['gender'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // 加密密码
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
     $sql = "INSERT INTO users (username, email, address, phone_number, gender, password) 
             VALUES ('$username', '$email', '$address', '$phone_number', '$gender', '$password')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "<script>alert('Registration successful!'); window.location.href='registration.html';</script>";
-} else {
-    echo "<script>alert('Error: " . $conn->error . "'); window.history.back();</script>";
-}
-
-
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . addslashes($conn->error);
+    }
 }
 
 $conn->close();
