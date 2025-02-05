fa5e0481134ed365be74f33faa696a8a36cc4912
CHENSHILING <1231200676@mmu.edu.my>
2025-02-05 12:29:39 +0800

    s

--- profile add child.php
+++ profile add child.php
@@ -1,3 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Child</title>
+    <link href="design.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+<div id="wrapper">
+
+    
+
+        <h1>Add Child</h1>
+
+        <form name="addChildForm" method="post" action="">
+
+            <p><label>Name:</label><input type="text" name="child_name" size="80"></p>
+         
+            <p><label>Gender:</label>
+                <select name="child_gender">
+                    <option value="Boy">Boy</option>
+                    <option value="Girl">Girl</option>
+                </select>
+            </p>
+            
+            <p><label>My Kid Number:</label><input type="text" name="kid_number" size="20"></p>
+
+            <p><label>Birthday:</label><input type="date" name="child_birthday"></p>
+            
+            <p><label>School:</label><input type="text" name="child_school" size="100"></p>
+
+            <p><label>Year:</label><input type="text" name="child_year" size="20"></p>
+            
+            <p><input type="submit" name="saveChildBtn" value="SAVE CHILD"></p>
+
+        </form>
+    
+    </div>
+    
+</div>
+
+</body>
+</html>
 
 <?php
 include("connectmyprofile.php");
