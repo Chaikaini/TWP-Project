f907cded7fa4b28e66dd6ea769d0138079036cc4
Chaikaini <1231201291@mmu.edu.my>
2025-02-05 15:48:22 +0800

    1

--- sql/tuition_centre.sql
+++ sql/tuition_centre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2025 at 06:56 AM
+-- Generation Time: Feb 05, 2025 at 08:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,25 @@ CREATE TABLE `comments` (
 
 INSERT INTO `comments` (`id`, `year`, `subject`, `rating`, `comment`, `created_at`) VALUES
 (1, 'Year 1', 'Malay', 5, 'Very Good！', '2025-02-05 05:18:06'),
-(2, 'Year 2', 'Math', 5, 'That is okay.', '2025-02-05 05:24:31');
+(2, 'Year 2', 'Math', 5, 'That is okay.', '2025-02-05 05:24:31'),
+(3, 'Year 1', 'Math', 4, 'Good:)', '2025-02-05 06:07:12'),
+(4, 'Year 1', 'English', 5, 'Nice\r\n', '2025-02-05 06:09:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `teacher` varchar(255) NOT NULL,
+  `year` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `duration` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -55,6 +73,12 @@ ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +86,13 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
