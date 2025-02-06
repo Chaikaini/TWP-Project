25c6edfb2a6f190954dd7f953215f647c3c381f1
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 00:58:52 +0800

    1

---
+++ comments.php
@@ -0,0 +1,27 @@
+<?php
+// db_connect.php 用于数据库连接
+include('db_connect.php');
+
+// 获取过滤条件
+$yearFilter = isset($_GET['year']) ? $_GET['year'] : 'All';
+
+// 查询评论数据
+if ($yearFilter == 'All') {
+    $sql = "SELECT * FROM comments ORDER BY created_at DESC";
+} else {
+    $sql = "SELECT * FROM comments WHERE year = '$yearFilter' ORDER BY created_at DESC";
+}
+
+$result = $conn->query($sql);
+
+// 将评论数据存储到数组中
+$comments = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $comments[] = $row;
+    }
+}
+
+// 关闭数据库连接
+$conn->close();
+?>
