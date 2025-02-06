44dd3cc45406b7ff34ef828b857e12838432cf8b
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-06 17:21:08 +0800

    1

--- sub.php
+++ sub.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$dbname = "subjects"; 
+$dbname = "tuition_centre"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
