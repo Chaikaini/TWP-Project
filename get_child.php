4ca5b47682c845f72645e0160f8ac72322cff8d2
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 21:56:55 +0800

    1

---
+++ get_child.php
@@ -0,0 +1,11 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "profile"; // 这里改成 profile 数据库
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("database connection fail: " . $conn->connect_error);
+}
+?>
