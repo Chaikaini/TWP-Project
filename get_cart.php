418bd97870b1e455772a266154adbf08813ca7c9
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 12:34:18 +0800

    1

--- get_cart.php
+++ get_cart.php
@@ -7,4 +7,27 @@ if (!empty($_SESSION["cart"])) {
 } else {
     echo json_encode([]);
 }
+
 ?>
+<?php
+// 你可以直接在这里硬编码数据作为测试
+$cart_data = [
+    [
+        "child" => "Yuna",
+        "subject" => "Year 1 English",
+        "teacher" => "Mr. John",
+        "price" => 85
+    ],
+    [
+        "child" => "Yuna",
+        "subject" => "Year 1 Math",
+        "teacher" => "Mr. David",
+        "price" => 85
+    ]
+];
+
+// 返回 JSON 格式的数据
+header('Content-Type: application/json');
+echo json_encode($cart_data);
+?>
+
