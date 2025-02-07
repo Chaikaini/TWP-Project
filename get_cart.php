76752a5bc2905ab70f9bb30db566e08a36b584f4
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 13:03:45 +0800

    1

--- get_cart.php
+++ get_cart.php
@@ -22,17 +22,28 @@ header('Content-Type: application/json');
 // 你的测试数据
 $cart_data = [
     [
-        "child" => "Yuna",
-        "subject" => "Year 1 English",
-        "teacher" => "Mr. John",
-        "price" => 85
-    ],
-    [
-        "child" => "David",
-        "subject" => "Year 1 Math",
-        "teacher" => "Mr. David",
-        "price" => 85
-    ]
+            {
+                image: 'img/english.jpg',
+                subject: 'English class',
+                teacher: 'Mr. John',
+                year: 'Year 1', 
+                price: 85,
+                time: 'Monday, 2.30pm-4.30pm',
+                capacity: '20/30',
+                student: 'Yuna',
+                startMonth: 0 // January
+            },
+            {
+                image: 'img/math.jpg',
+                subject: 'Math  class',
+                teacher: 'Mr. David',
+                year: 'Year 1', 
+                price: 85,
+                time: 'Wednesday, 2.30pm-4.30pm',
+                capacity: '20/30',
+                student: 'Yuna',
+                startMonth: 0
+            }
 ];
 
 // 确保没有额外的输出，直接返回 JSON 数据
