90657c406bfdd141c270d97df70aceffb8815697
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 23:28:25 +0800

    1

--- get_classdetails.php
+++ get_classdetails.php
@@ -12,9 +12,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// 获取课程数据，根据 class_name 和 part 来过滤
-$class_name = 'Year 1 English'; // 你可以根据需要修改这个值
-$sql = "SELECT * FROM classdetail WHERE class_name = '$class_name'";  // 查询指定课程的 Part A 和 Part B
+// 获取课程数据，根据 class_name 来过滤
+$class_name = isset($_GET['class_name']) ? $_GET['class_name'] : 'Year 1 English'; // 如果没有传递 class_name 参数，默认使用 'Year 1 English'
+
+$sql = "SELECT * FROM classdetail WHERE class_name = '$class_name'";  // 查询指定课程
 $result = $conn->query($sql);
 
 // 返回数据
@@ -30,5 +31,3 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
-
-
