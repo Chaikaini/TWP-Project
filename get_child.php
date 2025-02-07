cbfbe4b2557e25ec06626de716938ccda1777f96
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 23:08:44 +0800

    1

--- get_child.php
+++ get_child.php
@@ -12,7 +12,7 @@ $childId = isset($_GET['id']) ? $_GET['id'] : null; // 获取查询参数 'id'
 
 // 如果没有提供 id，默认返回 id=2 的孩子数据
 if ($childId === null) {
-    $childId = 2;
+    $childId = 1;
 }
 
 // 使用指定的 id 来查询孩子信息
