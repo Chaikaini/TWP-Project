77f3f14ee0cd91ddfe7a51cced1559942fdb6aa0
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-07 13:31:27 +0800

    1

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,1 +1,15 @@
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $subject = $_POST["subject"];
+    $child = $_POST["child"];
 
+    if (!isset($_SESSION["cart"])) {
+        $_SESSION["cart"] = [];
+    }
+
+    $_SESSION["cart"][] = ["subject" => $subject, "child" => $child];
+
+    echo "Added $subject for $child to cart!";
+}
+?>
