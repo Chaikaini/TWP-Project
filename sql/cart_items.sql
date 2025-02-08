b3d41f4c79eef4e850e728f6c32fcb67ce80bac6
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-08 13:50:58 +0800

    1

--- sql/cart_items.sql
+++ sql/cart_items.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2025 at 04:17 AM
+-- Generation Time: Feb 08, 2025 at 06:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,16 +31,17 @@ CREATE TABLE `cart_items` (
   `id` int(11) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `child` varchar(255) NOT NULL
+  `child` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cart_items`
 --
 
-INSERT INTO `cart_items` (`id`, `subject`, `price`, `child`) VALUES
-(1, 'Year 1 English', 510.00, 'John Doe'),
-(2, 'Year 1 Melayu', 510.00, 'John Doe');
+INSERT INTO `cart_items` (`id`, `subject`, `price`, `child`, `image`) VALUES
+(1, 'Year 1 English', 510.00, 'John Doe', 'http://localhost/Fyp/TWP-Project/img/english.jpg'),
+(2, 'Year 1 Malay', 510.00, 'John Doe', 'http://localhost/Fyp/TWP-Project/img/malay.jpg');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +61,7 @@ ALTER TABLE `cart_items`
 -- AUTO_INCREMENT for table `cart_items`
 --
 ALTER TABLE `cart_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
