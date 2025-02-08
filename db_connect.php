6215d45744927b783a33e73a85621414242cf2be
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-08 11:42:35 +0800

    1

--- db_connect.php
+++ db_connect.php
@@ -8,4 +8,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("database connection fail: " . $conn->connect_error);
 }
+
+function dbConnect() {
+    $conn = new mysqli("localhost", "root", "", "tuition_centre");
+
+    if ($conn->connect_error) {
+        die(json_encode(['status' => 'error', 'message' => 'Database connection failed']));
+    }
+
+    return $conn;
+}
 ?>
