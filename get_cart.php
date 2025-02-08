75fa0aa72504f393523ae6dec15649ff9915ce1e
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-08 11:57:47 +0800

    1

--- get_cart.php
+++ get_cart.php
@@ -2,6 +2,9 @@
 include('db_connect.php'); // 确保连接数据库
 header('Content-Type: application/json');
 
+ini_set('display_errors', 1); // 显示所有错误
+error_reporting(E_ALL); // 显示所有错误
+
 $conn = dbConnect();
 
 // 查询数据库中的购物车数据
@@ -11,6 +14,7 @@ $result = $conn->query($sql);
 $cart = [];
 while ($row = $result->fetch_assoc()) {
     $cart[] = $row;
+}
 
 // 关闭数据库连接
 $conn->close();
