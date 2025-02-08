33b3f1e5ddc9b4d100acb7e3b1bec2be747952c0
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-08 15:37:47 +0800

    1

--- fetch_parents.php
+++ fetch_parents.php
@@ -3,7 +3,7 @@
 $servername = "localhost";  // 数据库主机（通常是 localhost）
 $username = "root";         // 数据库用户名（替换为你的用户名）
 $password = "";             // 数据库密码（替换为你的密码）
-$dbname = "tution_centre";  // 你的数据库名称
+$dbname = "tution_centre";  // 确保数据库名称正确
 
 // 创建连接
 $conn = new mysqli($servername, $username, $password, $dbname);
