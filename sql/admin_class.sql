ecc7e7aedbffae35377fae5aa732d515e6911523
CHENSHILING <1231200676@mmu.edu.my>
2025-02-10 10:09:33 +0800

    q

--- sql/admin_class.sql
+++ sql/admin_class.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2025 at 01:27 PM
+-- Generation Time: Feb 10, 2025 at 03:08 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,12 +42,12 @@ CREATE TABLE `admin_class` (
 --
 
 INSERT INTO `admin_class` (`class_id`, `subject_id`, `year`, `day`, `time`, `teacher`, `capacity`) VALUES
-('Eng0001', 11245, 'Year 1', 'Monday', '2:30pm - 4:30pm', 'Mr.John', '20/30'),
-('Eng2001', 22534, 'Year 2', 'Monday', '5:00pm - 7:00pm', 'Mr.John', '20/30'),
-('Mat0003', 11132, 'Year 1', 'Wednesday', '2:30pm - 4:30pm', 'Mr.David', '20/30'),
-('Mat2003', 22134, 'Year 2', 'Wednesday', '5:00pm - 7:00pm', 'Mr.David', '20/30'),
-('Mly0002', 11351, 'Year 1', 'Tuesday', '2:30pm - 4:30pm', 'Ms.Lily', '20/30'),
-('Mly2002', 22345, 'Year 2', 'Tuesday', '5:00pm - 7:00pm', 'Ms.Lily', '20/30');
+('Eng0001', 11245, 'Year 1', 'Monday', '2:30pm - 4:30pm', 'Mr. John', '0/30'),
+('Eng2001', 22534, 'Year 2', 'Monday', '5:00pm - 7:00pm', 'Mr. John', '0/30'),
+('Mat0003', 11132, 'Year 1', 'Wednesday', '2:30pm - 4:30pm', 'Mr. David', '3/30'),
+('Mat2003', 22134, 'Year 2', 'Wednesday', '5:00pm - 7:00pm', 'Mr. David', '0/30'),
+('Mly0002', 11351, 'Year 1', 'Tuesday', '2:30pm - 4:30pm', 'Ms. Lily', '0/30'),
+('Mly2002', 22345, 'Year 2', 'Tuesday', '5:00pm - 7:00pm', 'Ms. Lily', '1/30');
 
 --
 -- Indexes for dumped tables
@@ -59,16 +59,6 @@ INSERT INTO `admin_class` (`class_id`, `subject_id`, `year`, `day`, `time`, `tea
 ALTER TABLE `admin_class`
   ADD PRIMARY KEY (`class_id`),
   ADD KEY `subject_id` (`subject_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `admin_class`
---
-ALTER TABLE `admin_class`
-  ADD CONSTRAINT `admin_class_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `admin_subject` (`subject_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
