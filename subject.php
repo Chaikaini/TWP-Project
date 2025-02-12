8ebeb0c4310f4c02820434339f509069efaf759e
LIMENXI <1231200870@student.mmu.edu.my>
2025-02-12 08:42:27 +0800

    1

--- subject.php
+++ subject.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json'); // 设置 JSON 头部
 include 'db_connect.php'; // 连接数据库
 
-$year = isset($_GET['year']) ? $_GET['year'] : 'Year 1'; // 获取参数，默认为 Year 1
+$year = isset($_GET['year']) ? $_GET['year'] : 'Year 3'; // 获取参数，默认为 Year 1
 
 $sql = "SELECT name, image, teacher, price, rating, page FROM subjects WHERE TRIM(LOWER(year)) = LOWER(TRIM(?))";
 $stmt = $conn->prepare($sql);
