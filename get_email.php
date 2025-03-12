91ff5ea083c76ac90d83b30847d9a755bca429d7
LIMENXI <1231200870@student.mmu.edu.my>
2025-03-12 14:48:36 +0800

    1

--- get_email.php
+++ get_email.php
@@ -2,18 +2,13 @@
 $servername = "127.0.0.1";  // 数据库服务器地址
 $username = "root";         // 数据库用户名
 $password = "";             // 数据库密码（假设为空）
-$dbname = "user_information";  // 数据库名称（这个是你提供的数据库名称）
+$dbname = "user_information";  // 数据库名称
 
-// 创建连接
+// 创建数据库连接
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 检查连接
+// 检查连接是否成功
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } else {
-        echo "Connected successfully!";
-    }
-    
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
