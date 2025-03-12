8527e7a4833d46f194c1d2d4fa3558eb50c8ef10
LIMENXI <1231200870@student.mmu.edu.my>
2025-03-12 14:45:03 +0800

    1

--- get_email.php
+++ get_email.php
@@ -9,6 +9,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 检查连接
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected successfully!";
+    }
+    
 }
 ?>
